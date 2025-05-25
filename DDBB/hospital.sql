43e6561417f199dc182b9300e170b9aad1895558
fernando <mkiii@hotmail.es>
2025-05-25 18:35:58 +0200

    fixed

--- DDBB/hospital.sql
+++ DDBB/hospital.sql
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 
 --
 -- Base de datos: `hospital3`
+CREATE DATABASE IF NOT EXISTS hospital;
+USE hospital;
 --
 
 -- --------------------------------------------------------
