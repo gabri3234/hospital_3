6771fdd75418248bfd6363ae66008ce85312b95b
fernando <mkiii@hotmail.es>
2025-05-26 13:11:09 +0200

    db export with data to test

--- DDBB/hospital.sql
+++ DDBB/hospital.sql
@@ -1,10 +1,27 @@
--- Base de datos: `hospital3`
+-- MySQL dump 10.13  Distrib 8.0.42, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: hospital
+-- ------------------------------------------------------
+-- Server version	8.0.42-0ubuntu0.24.04.1
 
-DROP DATABASE IF EXISTS `hospital`;
-CREATE DATABASE `hospital` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `hospital`;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Tabla: camas
+--
+-- Table structure for table `camas`
+--
+
+DROP TABLE IF EXISTS `camas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `camas` (
   `id` int NOT NULL AUTO_INCREMENT,
   `habitacion_id` int NOT NULL,
@@ -12,46 +29,109 @@ CREATE TABLE `camas` (
   `estado` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `habitacion_id` (`habitacion_id`),
-  KEY `paciente_id` (`paciente_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  KEY `paciente_id` (`paciente_id`),
+  CONSTRAINT `camas_ibfk_1` FOREIGN KEY (`habitacion_id`) REFERENCES `habitaciones` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `camas_ibfk_2` FOREIGN KEY (`paciente_id`) REFERENCES `pacientes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Tabla: habitaciones
+--
+-- Dumping data for table `camas`
+--
+
+LOCK TABLES `camas` WRITE;
+/*!40000 ALTER TABLE `camas` DISABLE KEYS */;
+INSERT INTO `camas` VALUES (1,1,NULL,'LIBRE'),(2,1,NULL,'LIBRE'),(3,1,1,'OCUPADA'),(4,1,NULL,'LIBRE'),(5,1,NULL,'LIBRE'),(6,1,NULL,'LIBRE'),(7,1,NULL,'LIBRE'),(8,1,NULL,'LIBRE'),(9,2,NULL,'LIBRE'),(10,2,NULL,'LIBRE'),(11,2,2,'OCUPADA'),(12,2,NULL,'LIBRE'),(13,3,4,'OCUPADA'),(14,3,NULL,'LIBRE'),(15,3,NULL,'LIBRE'),(16,3,NULL,'LIBRE'),(17,3,NULL,'LIBRE'),(18,3,NULL,'LIBRE'),(19,3,NULL,'LIBRE');
+/*!40000 ALTER TABLE `camas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `habitaciones`
+--
+
+DROP TABLE IF EXISTS `habitaciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `habitaciones` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) DEFAULT NULL,
+  `nombre` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `capacidad` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Tabla: pacientes
+--
+-- Dumping data for table `habitaciones`
+--
+
+LOCK TABLES `habitaciones` WRITE;
+/*!40000 ALTER TABLE `habitaciones` DISABLE KEYS */;
+INSERT INTO `habitaciones` VALUES (1,NULL,8),(2,NULL,6),(3,NULL,11),(5,NULL,4);
+/*!40000 ALTER TABLE `habitaciones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pacientes`
+--
+
+DROP TABLE IF EXISTS `pacientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `pacientes` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) NOT NULL,
-  `apellido` varchar(100) NOT NULL,
-  `dni` varchar(20) NOT NULL,
+  `nombre` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `apellido` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `dni` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
   `gravedad` int NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `dni` (`dni`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Tabla: usuarios
+--
+-- Dumping data for table `pacientes`
+--
+
+LOCK TABLES `pacientes` WRITE;
+/*!40000 ALTER TABLE `pacientes` DISABLE KEYS */;
+INSERT INTO `pacientes` VALUES (1,'Carlos','Garcia','48729816H',2),(2,'Pepe','Gonzalez','19478394L',1),(3,'Laura','Marin','76482319K',1),(4,'Adrian','Lopez','572399087G',3),(5,'Alejandra','Martinez','43761215F',3);
+/*!40000 ALTER TABLE `pacientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuarios` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `username` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `isAdmin` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Inserción de usuarios
-INSERT INTO `usuarios` (`id`, `username`, `password`, `isAdmin`) VALUES
-(1, 'admin', '$2a$10$aq/BFAimr5bZ/7ItLVp/sOalv74E3PZOk0sRxO3e8.O0IahpgCosq', 1),
-(2, 'FranciscoMED', '$2a$10$9rTg/2/03Ws4wNLkhO9i0.E/RIWIYIJ3yTgqRiah5OoTj8tFyPf2W', 0),
-(3, 'CristinaMED', '$2a$10$sWZ99WbgKKi.NlcnzGiMdeAE/smFRHCPlYzTjILAdrkU30iGYlKR6', 0),
-(4, 'GabrielMED', '$2a$10$vFL67hqFJSV8ewXm7l7jV.t4G42ghuIRiVFXUAduduGyNLqoCUvKG', 0);
+--
+-- Dumping data for table `usuarios`
+--
 
--- Relaciones
-ALTER TABLE `camas`
-  ADD CONSTRAINT `camas_ibfk_1` FOREIGN KEY (`habitacion_id`) REFERENCES `habitaciones` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `camas_ibfk_2` FOREIGN KEY (`paciente_id`) REFERENCES `pacientes` (`id`);
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (8,'admin','$2a$12$wblJmiBuP4xaW6bFlHkeHe80Xz78zaSW/NRY7J4liRBkvxf9Ep5qS',1),(9,'FranciscoMED','$2a$12$HpL6VUiC1YibHtgm.rlNlueI1.aGtW65Ea6kAFYFVKHHodps0u5am',0),(10,'CristinaMED','$2a$12$rYVFn3S1NmuwDLth/Ijcxua1DlyyAsFsByCWD0QC6XAO9eDGr.AOu',0),(11,'GabrielMED','$2a$12$CXAa16Rk36vqU4R6.vdEB.n7LFPcUk7ytDpRq.IqIv/nu7AEFCaYG',0);
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-26 13:08:54
